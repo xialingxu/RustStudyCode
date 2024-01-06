535441c46686fb55084f38c83007b970f06998d5
xialingxu <jeffreyxu@yeah.net>
2024-01-06 16:55:45 +0800

    new file:   add/Cargo.lock 	new file:   add/Cargo.toml 	new file:   add/add-one/Cargo.toml 	new file:   add/add-one/src/lib.rs 	new file:   add/adder/Cargo.toml 	new file:   add/adder/src/main.rs 	new file:   add/target/.rustc_info.json 	new file:   add/target/CACHEDIR.TAG 	new file:   add/target/debug/.cargo-lock 	new file:   add/target/debug/.fingerprint/adder-e68b4d129d6d40b4/bin-adder 	new file:   add/target/debug/.fingerprint/adder-e68b4d129d6d40b4/bin-adder.json 	new file:   add/target/debug/.fingerprint/adder-e68b4d129d6d40b4/dep-bin-adder 	new file:   add/target/debug/.fingerprint/adder-e68b4d129d6d40b4/invoked.timestamp 	new file:   add/target/debug/adder.d 	new file:   add/target/debug/adder.exe 	new file:   add/target/debug/adder.pdb 	new file:   add/target/debug/deps/adder.d 	new file:   add/target/debug/deps/adder.exe 	new file:   add/target/debug/deps/adder.pdb 	new file:   add/target/debug/incremental/adder-33omwe41a8bsc/s-gs7evlm7xi-8zf2g7-1kluha8t6xgsdml4zti3fjtyv/14mz0ttysfpzu3er.o 	new file:   add/target/debug/incremental/adder-33omwe41a8bsc/s-gs7evlm7xi-8zf2g7-1kluha8t6xgsdml4zti3fjtyv/185qnho9fh4o1q41.o 	new file:   add/target/debug/incremental/adder-33omwe41a8bsc/s-gs7evlm7xi-8zf2g7-1kluha8t6xgsdml4zti3fjtyv/1kirqasc7qmgrc0g.o 	new file:   add/target/debug/incremental/adder-33omwe41a8bsc/s-gs7evlm7xi-8zf2g7-1kluha8t6xgsdml4zti3fjtyv/4z5lhfmu801u95c8.o 	new file:   add/target/debug/incremental/adder-33omwe41a8bsc/s-gs7evlm7xi-8zf2g7-1kluha8t6xgsdml4zti3fjtyv/dep-graph.bin 	new file:   add/target/debug/incremental/adder-33omwe41a8bsc/s-gs7evlm7xi-8zf2g7-1kluha8t6xgsdml4zti3fjtyv/jfi9q081zrk3lm8.o 	new file:   add/target/debug/incremental/adder-33omwe41a8bsc/s-gs7evlm7xi-8zf2g7-1kluha8t6xgsdml4zti3fjtyv/l3szw52fq3347xd.o 	new file:   add/target/debug/incremental/adder-33omwe41a8bsc/s-gs7evlm7xi-8zf2g7-1kluha8t6xgsdml4zti3fjtyv/query-cache.bin 	new file:   add/target/debug/incremental/adder-33omwe41a8bsc/s-gs7evlm7xi-8zf2g7-1kluha8t6xgsdml4zti3fjtyv/work-products.bin 	new file:   add/target/debug/incremental/adder-33omwe41a8bsc/s-gs7evlm7xi-8zf2g7.lock

---
+++ add/target/debug/adder.d
@@ -0,0 +1,1 @@
+D:\Code\RustStudyCode\add\target\debug\adder.exe: D:\Code\RustStudyCode\add\adder\src\main.rs
---
+++ add/target/debug/deps/adder.d
@@ -0,0 +1,5 @@
+D:\Code\RustStudyCode\add\target\debug\deps\adder.exe: adder\src\main.rs
+
+D:\Code\RustStudyCode\add\target\debug\deps\adder.d: adder\src\main.rs
+
+adder\src\main.rs:
