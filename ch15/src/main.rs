7201729424bbb1a437df1e943f9049595f30c6df
xialingxu <jeffreyxu@yeah.net>
2024-01-07 16:29:20 +0800

    new file:   ch15/.gitignore 	new file:   ch15/Cargo.lock 	new file:   ch15/Cargo.toml 	new file:   ch15/src/main.rs

---
+++ ch15/src/main.rs
@@ -0,0 +1,20 @@
+use crate::List::{Cons, Nil};
+
+fn main() {
+    let b = Box::new(5);
+    println!("b = {}", b);
+
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    let x= 5;
+    let y = &x;
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
+enum List {
+    // Cons(i32, List),
+    Cons(i32, Box<List>),
+    Nil,
+}
