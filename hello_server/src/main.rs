cadbe78992f319d8398919df404f87ef2854d5f1
xialingxu <jeffreyxu@yeah.net>
2024-03-10 11:31:25 +0800

    modified:   .vscode/settings.json 	new file:   hello_server/.gitignore 	new file:   hello_server/404.html 	new file:   hello_server/Cargo.lock 	new file:   hello_server/Cargo.toml 	new file:   hello_server/hello.html 	new file:   hello_server/src/main.rs

---
+++ hello_server/src/main.rs
@@ -0,0 +1,41 @@
+use std::{io::Read, net::{TcpListener, TcpStream}};
+use std::io::prelude::*;
+
+
+fn main() {
+    let listener=TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming(){
+        let _stream=stream.unwrap();
+
+        handle_connection(_stream);
+
+        //println!("Connection established");
+    }
+
+   
+}
+
+fn handle_connection(mut stream:TcpStream){
+    let mut buffer=[0;512];
+    //把stream中的数据读取到buffer中
+    stream.read(&mut buffer).unwrap();
+    println!("Request: {}",String::from_utf8_lossy(&buffer[..]));
+
+    let get=b"GET / HTTP/1.1\r\n";
+
+    let (status_line,filename)=if buffer.starts_with(get){
+        ("HTTP/1.1 200 OK\r\n\r\n","hello.html")
+    }
+    else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n","404.html")
+    };
+
+    let contents=std::fs::read_to_string(filename).unwrap();
+    let response=format!("{}{}",status_line,contents);
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();     
+
+   
+
+}
